aebd815a05abb735325f3efe25e4dff98fba9259
Jason Gorman <jason.gorman@codemanship.com>
2020-09-06 10:14:51 +0100

    Fixed test for green CI build and start of video prep

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -6,7 +6,7 @@ from src.dummy import Dummy
 class DummyTest(unittest.TestCase):
     def test_something(self):
         dummy = Dummy()
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
