f1ce79d037ff52b0d89d62b4073a44e9fbec19ad
Jason Gorman <jason.gorman@codemanship.com>
2020-09-06 09:32:11 +0100

    Added dummy domain object to test package resolution on build/test

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -1,8 +1,11 @@
 import unittest
 
+from src.dummy import Dummy
+
 
 class DummyTest(unittest.TestCase):
     def test_something(self):
+        dummy = Dummy()
         self.assertEqual(True, True)
 
 
